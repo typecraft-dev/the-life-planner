a527321142d8f94ae93531e3c79fa5911a741cb4
typecraft <chris@typecraft.dev>
2024-11-17 11:02:45 -0500

    Add group model and update fixutres

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ApplicationRecord
+  belongs_to :group
+
   STATUSES = %w[in_progress completed archived].freeze
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_17_153045) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_17_155552) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   # Custom types defined in this database.
   # Note that some types may not work with other database engines. Be careful if changing database.
   create_enum "task_status", ["in_progress", "completed", "archived"]
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "sessions", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -33,6 +39,8 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_17_153045) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.enum "status", default: "in_progress", null: false, enum_type: "task_status"
+    t.bigint "group_id", null: false
+    t.index ["group_id"], name: "index_tasks_on_group_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,8 +48,12 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_17_153045) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "group_id", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
+    t.index ["group_id"], name: "index_users_on_group_id"
   end
 
   add_foreign_key "sessions", "users"
+  add_foreign_key "tasks", "groups"
+  add_foreign_key "users", "groups"
 end
