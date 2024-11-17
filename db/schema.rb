a3b93c861bac8f1be1ab128fa855ff000a904a2e
typecraft <chris@typecraft.dev>
2024-11-17 10:55:06 -0500

    Add authentication with the authentication generator!

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_17_151616) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_17_153045) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   # Custom types defined in this database.
   # Note that some types may not work with other database engines. Be careful if changing database.
   create_enum "task_status", ["in_progress", "completed", "archived"]
+
+  create_table "sessions", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "ip_address"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
@@ -25,4 +34,14 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_17_151616) do
     t.datetime "updated_at", null: false
     t.enum "status", default: "in_progress", null: false, enum_type: "task_status"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
+  add_foreign_key "sessions", "users"
 end
