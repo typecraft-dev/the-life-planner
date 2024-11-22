22eeb5555d965e945af26810ead0b5243912008e
typecraft <chris@typecraft.dev>
2024-11-22 13:26:20 -0500

    Add mission control, and the admin controller to watch jobs process

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module TheLifePlanner
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
+    config.mission_control.jobs.base_controller_class = "AdminController"
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
